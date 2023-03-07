07d0e00aeebb296efca70c2fd23792a2a108bd99
Lucia Nishimiya <lucianishimiya@gmail.com>
2023-03-07 15:10:28 -0300

    *c sonroja

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,9 @@
-module.exports = (client) => {
-  console.log("[Servidor iniciado correctamente]: https://mondongo.cf/ | " + client.user.tag );
-};
+module.exports = async client => {
+    let totalUsers = client.guilds.cache.reduce((users , value) => users + value.memberCount, 0);
+    let totalGuilds = client.guilds.cache.size
+    let totalChannels = client.channels.cache.size
+
+   
+    client.user.setPresence({ activity: { name: `MC.ZUROS.XYZ 👈 EL MEJOR SERVIDOR DE MINECRAFT`, type: "PLAYING" }, status: "online" });
+    console.log("[Servidor iniciado correctamente]: https://mondongo.cf/ | " + client.user.tag );
+}
